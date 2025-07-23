e3fcd5bef58b2ea46cb890101750217f71a38ef6
Przemek <przemek@192.168.1.11>
2025-07-23 18:35:44 +0200

    update transaction support + delete support

--- src/PnL.hs
+++ src/PnL.hs
@@ -24,6 +24,7 @@ import qualified Data.Text as T
 import qualified Data.List.NonEmpty as NE
 import Session (ensureSession)
 import Validation
+import Network.HTTP.Types (ok200)
 
 newtype TransactionId = TransactionId Int64
     deriving (Show, Eq, Generic)
@@ -78,8 +79,8 @@ minValue = -999999.99
 validateName :: Text -> Validation (NonEmpty FieldError) Text
 validateName fName = validateNameNotEmpty fName <* validateNameTooLong fName
     where
-        validateNameNotEmpty x = x <$ failureIf (T.null fName) (FieldError "descrition" EmptyDescription)
-        validateNameTooLong x = x <$ failureIf (T.length fName > 256) (FieldError "descrition" DescriptionTooLong)
+        validateNameNotEmpty x = x <$ failureIf (T.null fName) (FieldError "description" EmptyDescription)
+        validateNameTooLong x = x <$ failureIf (T.length fName > 256) (FieldError "description" DescriptionTooLong)
 
 validateValue :: Double -> Validation (NonEmpty FieldError) Double
 validateValue val = validateMaxValue val <* validateMinValue val
@@ -93,15 +94,19 @@ validateForm Form {..} = Form formId <$> validateName formDescription <*> valida
 pnl :: ScottyT App ()
 pnl = do
     Scotty.get "/pnl" $ ensureSession >> Scotty.rescue transactions logSqlError
-    Scotty.get "/add-transaction" $ ensureSession >> addTransactionForm (Form Nothing "" 0) Nothing
-    Scotty.post "/add-transaction" $ ensureSession >> addTransactionHandler
-    Scotty.get "/success" successPage
-    Scotty.get "/error" errorPage
+    Scotty.get "/pnl/add-transaction" $ ensureSession >> transactionForm (Form Nothing "" 0) Nothing
+    Scotty.post "/pnl/add-transaction" $ ensureSession >> addTransactionHandler
+    Scotty.get "/pnl/edit-transaction/:id" $ ensureSession >> editTransactionHandler
+    Scotty.post "/pnl/edit-transaction/:id" $ ensureSession >> updateTransactionHandler
+    Scotty.delete "/pnl/delete-transaction/:id" $ ensureSession >> deleteTransactionHandler
+    Scotty.get "/pnl/success" successPage
+    Scotty.get "/pnl/transaction-deleted" deletedPage
+    Scotty.get "/pnl/error" errorPage
 
 logSqlError :: SomePostgreSqlException -> ActionT App ()
 logSqlError e = do
     lift $ logErrorN $ toText $ displayException e
-    Scotty.redirect "/error"
+    Scotty.redirect "/pnl/error"
 
 errorPage :: ActionT App ()
 errorPage = do
@@ -114,7 +119,15 @@ successPage = do
     Scotty.html . renderHtml . layout $ do
         h1 "Transakcja zapisana"
         ul $ do
-            li $ a ! href "/add-transaction" $ "Dodaj kolejną"
+            li $ a ! href "/pnl/add-transaction" $ "Dodaj kolejną"
+            li $ a ! href "/pnl" $ "Wróć do listy"
+
+deletedPage :: ActionT App ()
+deletedPage = do
+    layout <- layoutM
+    Scotty.html . renderHtml . layout $ do
+        h1 "Transakcja usunięta"
+        ul $ do
             li $ a ! href "/pnl" $ "Wróć do listy"
 
 printScientificValue :: Scientific -> Text
@@ -129,23 +142,28 @@ transactions = do
     let total = sum $ transactionValue <$> allTransactions
     Scotty.html . renderHtml $ layout $ do
         h1 "Lipiec, 2025"
-        a ! href "/add-transaction" $ "Dodaj"
+        a ! href "/pnl/add-transaction" $ "Dodaj"
         table ! id "transactions" $ do
             thead $ tr $ do
                 th ! scope "col" $ "#"
                 th ! scope "col" $ "Nazwa"
                 th ! scope "col" $ "Wartość"
+                th mempty
             tbody $ do
                 forM_ allTransactions toRow
                 tfoot $ do
                     th ! scope "row" ! colspan "2" $ "Suma:"
                     th $ text (printScientificValue total)
+                    th mempty
     where
+        editLink :: TransactionId -> Html
+        editLink tId = a ! href ("/pnl/edit-transaction/" <> toValue (showt tId)) $ "Edytuj"
         toRow :: Transaction -> Html
         toRow Transaction{..} = tr $ do
             th ! scope "col" $ text (showt transactionId)
             td $ text description
             td $ text (printScientificValue transactionValue)
+            td $ editLink transactionId
         stmt = [sql|
             SELECT id, name, value FROM transactions ORDER BY id DESC;
         |]
@@ -160,13 +178,13 @@ addTransactionHandler = do
     either (handleError formData) handleSucces result
     where
         handleSucces :: TransactionId -> ActionT App ()
-        handleSucces _ = Scotty.redirect "/success"
+        handleSucces _ = Scotty.redirect "/pnl/success"
 
         handleError :: Form -> NonEmpty FieldError -> ActionT App ()
         handleError f errs = do
             -- htmx does not swap content if response ststus is 4*/5*
             -- Scotty.status badRequest400
-            addTransactionForm f $ Just errs
+            transactionForm f $ Just errs
 
 addTransaction :: AppEnv -> Form -> IO (Either (NonEmpty FieldError) TransactionId)
 addTransaction AppEnv{..} formData = do
@@ -175,19 +193,68 @@ addTransaction AppEnv{..} formData = do
             [Only tid] <- query conn stmt (formDescription, formValue)
             return $ Right (tid :: TransactionId)
         Failure errs -> return $ Left errs
-    -- Scotty.redirect "/pnl"
     where
         stmt = [sql|
             INSERT INTO transactions (name, value, created_at) VALUES (?, ?, transaction_timestamp()) RETURNING id
         |]
 
-addTransactionForm :: Form -> Maybe (NonEmpty FieldError) -> ActionT App ()
-addTransactionForm Form{..} errors = do
+editTransactionHandler :: ActionT App ()
+editTransactionHandler = do
+    paramId <- TransactionId <$> Scotty.captureParam @Int64 "id"
+    env <- lift ask
+    Transaction{..} <- liftIO $ getFromDb env paramId
+    let formData = Form (Just transactionId) description $ Scientific.toRealFloat transactionValue
+    transactionForm formData Nothing
+    where
+        getFromDb :: AppEnv -> TransactionId -> IO Transaction
+        getFromDb AppEnv{..} paramId = do
+            withResource connPool $ \conn -> do
+                [entity] <- query conn selectStmt $ Only paramId
+                return entity
+        selectStmt = [sql|
+            SELECT id, name, value FROM transactions WHERE id = ?;
+        |]
+
+updateTransactionHandler :: ActionT App ()
+updateTransactionHandler = do
+    formData <- (Form . Just . TransactionId <$> Scotty.formParam "id")
+        <*> Scotty.formParam "description"
+        <*> Scotty.formParam "value"
+    env <- lift ask
+    result <- liftIO $ updateTransaction env formData
+    either (handleError formData) (const (handleSucces formData)) result
+    where
+        handleSucces :: Form -> ActionT App ()
+        handleSucces fd = transactionForm fd Nothing
+
+        handleError :: Form -> NonEmpty FieldError -> ActionT App ()
+        handleError f errs = do
+            -- TODO: change htmx config
+            -- htmx does not swap content if response ststus is 4*/5*
+            -- Scotty.status badRequest400
+            transactionForm f $ Just errs
+
+updateTransaction :: AppEnv -> Form -> IO (Either (NonEmpty FieldError) ())
+updateTransaction _ (Form Nothing _ _) = error "Can't update"
+updateTransaction AppEnv{..} formData@(Form (Just transId) _ _) = do
+    case validateForm formData of
+        Success Form{..} -> withResource connPool $ \conn -> do
+            _ <- execute conn stmt (formDescription, formValue, transId)
+            return $ Right ()
+        Failure errs -> return $ Left errs
+    where
+        stmt = [sql|
+            UPDATE transactions SET name = ?, value = ? WHERE id = ?;
+        |]
+
+transactionForm :: Form -> Maybe (NonEmpty FieldError) -> ActionT App ()
+transactionForm Form{..} errors = do
     layout <- layoutM
     Scotty.html . renderHtml $ layout $ do
         a ! href "/pnl" $ "Wróć"
         h1 "Nowa transakcja"
         form ! method "POST" $ do
+            whenJust formId $ \tId -> input ! name "id" ! type_ "hidden" ! value (toAttrVal tId)
             label $ do
                 "Nazwa"
                 textarea ! required "required" ! name "description" $ text formDescription
@@ -197,6 +264,7 @@ addTransactionForm Form{..} errors = do
                 input ! required "required" ! name "value" ! type_ "number" ! step "0.01" ! value (toValue formValue)
                 whenJust errors $ renderErrors "value"
             button ! type_ "submit" $ "Save"
+        whenJust formId $ \tid -> deleteLink tid
     where
         renderErrors :: Text -> NonEmpty FieldError -> Html
         renderErrors n es = flip whenNotNull renderErrors' $ NE.filter (\FieldError{..} -> fName == n) es
@@ -204,3 +272,35 @@ addTransactionForm Form{..} errors = do
         renderErrors' :: NonEmpty FieldError -> Html
         renderErrors' errs = ul $ forM_ errs (li . text . showt . fError)
 
+        deleteLink :: TransactionId -> Html
+        deleteLink tid = a
+            ! class_ "delete"
+            ! href "#"
+            ! customAttribute "hx-delete" ("/pnl/delete-transaction/" <> toAttrVal tid)
+            ! customAttribute "hx-target" "main"
+            ! customAttribute "hx-confirm" "Czy na pewno chcesz usunąć?"
+            $ "Usuń"
+
+deleteTransactionHandler :: Handler ()
+deleteTransactionHandler = do
+    env <- lift ask
+    transactionId <- TransactionId <$> Scotty.captureParam "id"
+    liftIO $ deleteTransaction env transactionId
+    Scotty.status ok200
+    Scotty.html . renderHtml $ do
+        h1 "Transakcja usunięta"
+        ul $ do
+            li $ a ! href "/pnl" $ "Wróć do listy"
+
+deleteTransaction :: AppEnv -> TransactionId -> IO ()
+deleteTransaction AppEnv{..} transactionId = do
+    withResource connPool $ \conn -> do
+        _ <- execute conn stmt $ Only transactionId
+        return ()
+    where
+        stmt = [sql|
+            DELETE FROM transactions WHERE id = ?;
+        |]
+
+toAttrVal :: (TextShow a) => a -> AttributeValue
+toAttrVal = toValue . showt
