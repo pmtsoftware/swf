0159107dd43bceacb715c5db8857bcbfa8e7dd77
Przemek <przemek@192.168.1.11>
2025-11-03 21:28:10 +0100

    webauth endpoints

--- src/Webauthn/Service.hs
+++ src/Webauthn/Service.hs
@@ -15,8 +15,8 @@ import qualified Crypto.WebAuthn as WA
 import qualified Data.Aeson.Encode.Pretty as AP
 import qualified Data.Text.IO as TIO
 import Data.Aeson (FromJSON, ToJSON, Value (String))
-import Webauthn.Database (userExists, queryCredentialEntryByCredential, insertUser, insertCredentialEntry)
-import Webauthn.PendingCeremonies (insertPendingRegistration, getPendingRegistration)
+import Webauthn.Database (userExists, queryCredentialEntryByCredential, insertUser, insertCredentialEntry, queryCredentialEntriesByUser, updateSignatureCounter)
+import Webauthn.PendingCeremonies (insertPendingRegistration, getPendingRegistration, insertPendingAuthentication, getPendingAuthentication)
 import Data.Validation (Validation (Failure, Success))
 import System.Hourglass (dateCurrent)
 
@@ -31,8 +31,8 @@ service :: ScottyT App ()
 service = do
     Scotty.post "/webauthn/register/begin" beginRegistration
     Scotty.post "/webauthn/register/complete" completeRegistration
-    Scotty.post "/webauthn/login/begin" undefined
-    Scotty.post "/webauthn/login/complete" undefined
+    Scotty.post "/webauthn/login/begin" beginLogin
+    Scotty.post "/webauthn/login/complete" completeLogin
 
 -- | Utility function for debugging. Creates a human-readable bytestring from
 -- any value that can be encoded to JSON. We use this function to provide a log
@@ -165,3 +165,117 @@ completeRegistration = do
     Scotty.liftAndCatchIO $ TIO.putStrLn $ "Register complete => " <> jsonText response
     Scotty.json response
 
+beginLogin :: Handler ()
+beginLogin = do
+    accountName <- WA.UserAccountName <$> Scotty.jsonData @Text
+    Scotty.liftAndCatchIO $ TIO.putStrLn $ "Login begin <= " <> jsonText accountName
+
+    AppEnv{..} <- lift ask
+    credentials <- Scotty.liftAndCatchIO $
+        withResource connPool $ \conn -> do
+            queryCredentialEntriesByUser conn accountName
+    when (null credentials) $ do
+        Scotty.liftAndCatchIO $ TIO.putStrLn "Login begin error: User not found"
+        Scotty.raiseStatus HTTP.status404 "User not found"
+
+    -- Create credential options from the credential retrieved from the database
+    -- and insert the options into the pending ceremonies. This server stores the
+    -- entire options, but this isn't actually necessary a fully spec complient
+    -- RP implementation. See the documentation of `WA.CredentialOptions` for
+    -- more information.
+    options <- Scotty.liftAndCatchIO $
+        insertPendingAuthentication pendingCeremonies $ \challenge -> do
+            WA.CredentialOptionsAuthentication
+                { WA.coaRpId = Nothing,
+                    WA.coaTimeout = Nothing,
+                    WA.coaChallenge = challenge,
+                    WA.coaAllowCredentials = fmap mkCredentialDescriptor credentials,
+                    WA.coaUserVerification = WA.UserVerificationRequirementPreferred,
+                    WA.coaExtensions = Nothing
+                }
+ 
+    -- Send credential options to the client
+    Scotty.liftAndCatchIO $ TIO.putStrLn $ "Login begin => " <> jsonText options
+    Scotty.json $ WA.wjEncodeCredentialOptionsAuthentication options
+    where
+        mkCredentialDescriptor :: WA.CredentialEntry -> WA.CredentialDescriptor
+        mkCredentialDescriptor WA.CredentialEntry {WA.ceCredentialId, WA.ceTransports} =
+            WA.CredentialDescriptor
+                { WA.cdTyp = WA.CredentialTypePublicKey
+                , WA.cdId = ceCredentialId
+                , WA.cdTransports = Just ceTransports
+                }
+
+-- | Completes the relying party's responsibilities of the authentication
+-- ceremony. Receives the credential from the client and performs the
+-- [authentication operation](https://www.w3.org/TR/webauthn-2/#sctn-verifying-assertion).
+-- If the operation succeeds, the user is logged in, and
+-- redirected to the @authenticated.html@ page.
+completeLogin :: Handler ()
+completeLogin = do
+    -- Receive the credential from the client
+    credential <- Scotty.jsonData
+    Scotty.liftAndCatchIO $ TIO.putStrLn $ "Raw login complete <= " <> jsonText credential
+
+    -- Decode credential
+    cred <- case WA.wjDecodeCredentialAuthentication credential of
+        Left err -> do
+            Scotty.liftAndCatchIO $ TIO.putStrLn $ "Login complete failed to decode request: " <> show err
+            fail $ show err
+        Right result -> pure result
+    Scotty.liftAndCatchIO $ TIO.putStrLn $ "Login complete <= " <> jsonText (WA.stripRawCredential cred)
+
+    AppEnv{..} <- lift ask
+    -- Retrieve stored options from the pendingOptions
+    options <-
+        Scotty.liftAndCatchIO (getPendingAuthentication pendingCeremonies cred) >>= \case
+            Left err -> do
+                Scotty.liftAndCatchIO $ TIO.putStrLn $ "Login complete problem with challenge: " <> jsonText (String $ show err)
+                Scotty.raiseStatus HTTP.status401 $ "Challenge error: " <> show err
+            Right result -> pure result
+
+    -- Check database for user, abort if user is unknown.
+    mentry <- Scotty.liftAndCatchIO $
+        withResource connPool $ \conn ->
+            queryCredentialEntryByCredential conn (WA.cIdentifier cred)
+    entry <- case mentry of
+        Nothing -> do
+            Scotty.liftAndCatchIO $ TIO.putStrLn "Login complete credential entry doesn't exist"
+            fail "Credential not found"
+        Just entry -> pure entry
+
+    -- Perform the verification of the credential. Abort if the credential could
+    -- not be verified.
+    let verificationResult =
+            WA.verifyAuthenticationResponse
+                (one origin)
+                rpIdHash
+                (Just (WA.ceUserHandle entry))
+                entry
+                options
+                cred
+    WA.AuthenticationResult newSigCount <- case verificationResult of
+        Failure errs@(err :| _) -> do
+            Scotty.liftAndCatchIO $ TIO.putStrLn $ "Login complete had errors: " <> show errs
+            fail $ show err
+        Success result -> pure result
+
+    -- Update signature counter in the database/abort if it was potentially cloned
+    case newSigCount of
+        WA.SignatureCounterZero ->
+            Scotty.liftAndCatchIO $
+            TIO.putStrLn "SignatureCounter is Zero"
+        (WA.SignatureCounterUpdated counter) ->
+            Scotty.liftAndCatchIO $ do
+            TIO.putStrLn $ "Updating SignatureCounter to: " <> show counter
+            withResource connPool $
+                \conn -> do
+                    _ <- updateSignatureCounter conn (WA.cIdentifier cred) counter
+                    return ()
+        WA.SignatureCounterPotentiallyCloned -> Scotty.raiseStatus HTTP.status401 "Signature Counter Cloned"
+
+    -- Set the login cookie and send the result to the server
+    -- setAuthenticatedAs db (WA.ceUserHandle entry)
+    let result = String "success"
+    Scotty.liftAndCatchIO $ TIO.putStrLn $ "Login complete => " <> jsonText result
+    Scotty.json result
